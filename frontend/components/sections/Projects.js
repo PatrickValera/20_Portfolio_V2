031bea6a1afaf89cdb3a124e342c1ecc5b175715
saish0 <patrickvalera50@yahoo.com>
2022-03-06 01:48:17 -0800

    2nd commit layout idea

--- frontend/components/sections/Projects.js
+++ frontend/components/sections/Projects.js
@@ -1,8 +1,40 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
-const Projects = () => {
+const Projects = ({ projects }) => {
   return (
-    <div>Projects</div>
+    <>
+      <Typography variant='h6'>Projects I've Built</Typography>
+      {projects.slice(0, 3).map((project, index) => (
+        <Box display='grid' className='fade' sx={{ gridTemplateColumns: 'repeat(12,1fr)', position: 'relative', bgcolor: 'red', mb: 4 }}>
+
+          {/* IMAGE HERE */}
+          <Box sx={{ gridArea: '1/1/-1/7', bgcolor: 'pink' }}>
+            <Box sx={{ bgcolor: 'pink' }}>
+            </Box>
+          </Box>
+
+          {/* TEXT HERE */}
+          <Box sx={{ gridArea: '1/6/-1/-1', bgcolor: 'green' }}>
+            <Typography variant='body1'>{project.title}</Typography>
+            {/* <Typography variant='body1'>{project.description}</Typography> */}
+            <Typography variant='body1'>Project Summary {index +1}</Typography>
+          </Box>
+        </Box>
+      ))}
+
+      <Typography variant='h6'>Other Projects</Typography>
+      <Box display='flex'sx={{flexWrap:'wrap',gap:2}}>
+        {projects.slice(3).map((project, index) => (
+          <Box className='fade' sx={{ p: 0, flex: '100px 1 1',minWidth:'33%', bgcolor:'primary.main' }}>
+            <Typography variant='body1'>{project.title}</Typography>
+            {/* <Typography variant='body1'>{project.description}</Typography> */}
+            <Typography variant='body1'>Project Summary {index +4}</Typography>
+          </Box>
+        ))}
+      </Box>
+
+    </>
   )
 }
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,28 +3,44 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { Box, Container } from '@mui/material'
-import Header from '../components/Header'
+import Header from '../components/sections/Header'
 import Introduction from '../components/sections/Introduction'
 import Projects from '../components/sections/Projects'
 import Footer from '../components/sections/Footer'
+import Profile from '../public/patrick.json'
+import { useEffect } from 'react'
+
 
 export default function Home({ setTheme }) {
+  const { projects } = Profile
+  useEffect(() => {
+    const items = document.querySelectorAll(".fade")
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting)
+      })
+    })
+
+    items.forEach(item => {
+      observer.observe(item)
+    })
+  },[])
   return (
     <Box sx={{ bgcolor: 'primary.light' }}>
       <Header setTheme={setTheme} />
       <Container maxWidth='lg'>
         <Introduction />
-        <Projects />
-        <Footer/>
+        <Projects projects={projects} />
+        <Footer />
       </Container>
-      <Box sx={{display:'flex',flexDirection:'column',position:'fixed',bottom:'0',left:'40px',border:'1px solid green'}}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', position: 'fixed', bottom: '0', left: '40px', border: '1px solid green' }}>
         <span>A</span>
         <span>B</span>
         <span>C</span>
         <span>D</span>
         <span>E</span>
       </Box>
-      <Box sx={{width:'0',display:'flex',flexDirection:'column',position:'fixed',bottom:'0',right:'40px',border:'1px solid green',overflowWrap:'break-word'}}>
+      <Box sx={{ width: '0', display: 'flex', flexDirection: 'column', position: 'fixed', bottom: '0', right: '40px', border: '1px solid green', overflowWrap: 'break-word' }}>
         <span>Email</span>
       </Box>
     </Box>
