5909cfa6799d33ec86e0ef05749af46fed9efdb5
saish0 <patrickvalera50@yahoo.com>
2022-03-08 15:48:34 -0800

    add looping animation

--- frontend/components/sections/Introduction.js
+++ frontend/components/sections/Introduction.js
@@ -1,15 +1,17 @@
 import { Button, Typography } from '@mui/material'
 import React from 'react'
 import { Box } from '@mui/material'
+import HeroAnimation from '../HeroAnimation'
 const Introduction = () => {
   return (
-    <Box display='flex' sx={{pl:4, height: '100vh', flexDirection: 'column', justifyContent: 'center', maxWidth:'800px' }}>
-      <Box className='fade'sx={{ maxWidth: '450px' }}>
+    <Box display='flex' sx={{ height: '100vh', alignItems: 'center',alignContent:'center',gap:4, justifyContent:'center',flexWrap:'wrap' }}>
+      <Box className='fade'sx={{ pl:4,flexGrow:'1'}}>
         <Typography variant='body1'> Hello, my name is</Typography>
         <Typography variant='h2'> Patrick Valera.</Typography>
-        <Typography variant='body1' gutterBottom> A Software Engineer student and a self-taught web developer. I aim to deepen my understanding of various technologies and build exceptional products.</Typography>
+        <Typography variant='body1' sx={{maxWidth:'450px'}} gutterBottom> A Software Engineer student and a self-taught web developer. I aim to deepen my understanding of various technologies and build exceptional products.</Typography>
         <Button variant='outlined'>View Resume</Button>
       </Box>
+      <HeroAnimation/>
     </Box>
   )
 }
