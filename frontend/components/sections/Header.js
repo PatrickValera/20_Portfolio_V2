7c5a68ace68ae20c667bf2fe988508b46749a772
saish0 <patrickvalera50@yahoo.com>
2022-03-07 20:35:49 -0800

    nav anim/style

--- frontend/components/sections/Header.js
+++ frontend/components/sections/Header.js
@@ -1,33 +1,34 @@
 import { Box, Button, Link, Switch, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Header = ({ isLight,setTheme }) => {
+  useEffect(()=>{
+
+  },[])
   return (
-    <Box component='header' sx={{p:1, position: 'sticky', top: '0', zIndex: 10 }}>
+    <Box component='header' sx={{ position: 'sticky', top: '0', zIndex: 10 }}>
       <Box
         sx={{
           px: 2,
+          py:1,
           position: 'absolute',
           bgcolor: 'background.paper',
           display: 'flex',
           alignItems: 'center',
           width: '100%',
+          overflow:'hidden'
         }}
       >
         <Typography variant='h6' sx={{ flexGrow: '1' }}>
           PV
         </Typography>
-        <Box component='nav' display='flex' sx={{ gap: 2 }}>
-          <Link
-            href='#projects'
-            underline='none'
-            sx={{ color: 'text.primary' }}
-          >
-            Works
-          </Link>
-          <Link href='#awards' underline='none' sx={{ color: 'text.primary' }}>
-            Awards
-          </Link>
+        <Box component='nav' display='flex' sx={{ gap: 2,flexDirection:{xs:'column',md:'row'},position:{xs:'absolute',md:'relative'},top:'0',right:{xs:'-200px',md:'unset'} }}>
+          {['ABOUT','PROJECTS','AWARDS','SKILLS'].map((section,index)=>(
+            <Link className='nav-link' href={`#${section.toLowerCase()}`} underline='none' sx={{color:'text.primary',fontWeight:'600'}}>
+              {section}
+              </Link>
+          ))}
+    
         </Box>
         <Switch
           checked={isLight}
