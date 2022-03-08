cd64ff19ec9d88974de80519e3e834b8131038e8
saish0 <patrickvalera50@yahoo.com>
2022-03-08 15:50:35 -0800

    fix-add HeroAnimation comp

---
+++ frontend/components/HeroAnimation.js
@@ -0,0 +1,95 @@
+import { Box, Button } from '@mui/material';
+import React, { useEffect, useRef, useState } from 'react'
+import { GrHtml5 } from 'react-icons/gr'
+import { IoLogoCss3 } from "react-icons/io";
+import { DiJavascript1 } from "react-icons/di";
+import { SiMongodb,SiExpress,SiVisualstudiocode,SiNextdotjs,SiRedux,SiMaterialui } from "react-icons/si";
+import { FaReact,FaNodeJs,FaSass } from "react-icons/fa";
+
+let props = { className: 'hero-icon' }
+let logos = [
+    <IoLogoCss3 {...props} />,
+    <GrHtml5 {...props} />,
+    <DiJavascript1 {...props} />,
+    <SiMongodb {...props} />,
+    <SiExpress {...props} />,
+    <FaReact {...props} />,
+    <FaNodeJs {...props} />,
+    <SiVisualstudiocode {...props} />,
+    <SiNextdotjs {...props} />,
+
+    <SiRedux {...props} />,
+    <FaSass {...props} />,
+    <SiMaterialui {...props} />,
+    
+
+]
+
+const HeroAnimation = () => {
+    // useEffect(() => {
+    //     const container = document.querySelector('.hero-animation')
+
+    //     const LastTileObserver = new IntersectionObserver(entries => {
+    //         const lastCard = entries[0]
+    //         if (!lastCard.isIntersecting) return
+    //         else {
+    //             console.log("LOAD NEW TILE")
+    //             let newTile = document.querySelector('.tile:last-child')
+    //             // console.log(newTile)
+    //             console.log(container)
+    //             container.append(newTile)
+    //             LastTileObserver.unobserve(lastCard.target)
+    //             LastTileObserver.observe(document.querySelector('.tile:last-child'))
+    //         }
+    //     }, {
+    //         root: container,
+    //         rootMargin: '0px'
+    //     }
+    //     )
+    //     LastTileObserver.observe(document.querySelector('.tile:last-child'))
+
+
+    //     // let container = document.querySelector('.hero-animation')
+    //     // console.log(container)
+    //     // container.scroll({
+    //     //     top: 1000,
+    //     //     behavior: 'smooth'
+    //     // });
+    // }, [])
+    let container = useRef()
+    const [num, setNum] = useState([1])
+    useEffect(() => {
+        setTimeout(() => {
+            console.log(num)
+            setNum(state => [...state, (state[state.length - 1] + 1)])
+            let newPos = (num.length - 1) * 400
+            console.log(newPos)
+            container.current.style.transform = `translateY(-${String(newPos)}px)`
+        }, 5000)
+    },[num])
+    return (
+        <>
+            {/* <Button onClick={() => {
+                console.log(num)
+                setNum(state => [...state, (state[state.length - 1] + 1)])
+                let newPos = (num.length - 1) * 500
+                console.log(newPos)
+                container.current.style.transform = `translateY(-${String(newPos)}px)`
+            }}>ADD</Button> */}
+            <Box className='hero-animation' sx={{ position: 'relative', flex: '25% 1 1',maxWidth:'300px', height: {xs:'150px',md:'500px'}, overflowY: 'hidden' }}>
+                <Box className='tiles-container' ref={container} sx={{ transition: 'all 5000ms linear', position: 'relative', transform: 'translateY(00px)' }}>
+                    {num.map(num => (<Box key={num} className='tile' sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                        {logos.map((component, index) => (
+                            <Box key={index} sx={{ p: 2, display: 'flex', flex: '80px 1 1', m: 1 }}>
+                                {component}
+                            </Box>
+                        ))}
+                    </Box>))}
+                </Box>
+            </Box>
+        </>
+
+    )
+}
+
+export default HeroAnimation
