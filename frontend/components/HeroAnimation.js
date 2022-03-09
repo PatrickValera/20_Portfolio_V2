ea4a2356e5c29c927fb1b30f9aecfda490cd1862
saish0 <patrickvalera50@yahoo.com>
2022-03-09 10:04:09 -0800

    looping anim

--- frontend/components/HeroAnimation.js
+++ frontend/components/HeroAnimation.js
@@ -1,15 +1,15 @@
 import { Box, Button } from '@mui/material';
 import React, { useEffect, useRef, useState } from 'react'
-import { GrHtml5 } from 'react-icons/gr'
+import { AiOutlineHtml5 } from 'react-icons/ai'
 import { IoLogoCss3 } from "react-icons/io";
-import { DiJavascript1 } from "react-icons/di";
-import { SiMongodb,SiExpress,SiVisualstudiocode,SiNextdotjs,SiRedux,SiMaterialui } from "react-icons/si";
-import { FaReact,FaNodeJs,FaSass } from "react-icons/fa";
+import { DiJavascript1, DiGit, DiHeroku } from "react-icons/di";
+import { SiMongodb, SiExpress, SiVisualstudiocode, SiNextdotjs, SiRedux, SiMaterialui, SiVercel } from "react-icons/si";
+import { FaReact, FaNodeJs, FaSass } from "react-icons/fa";
 
-let props = { className: 'hero-icon' }
+let props = { className: 'hero-icon', }
 let logos = [
     <IoLogoCss3 key='1'{...props} />,
-    <GrHtml5 key='2'{...props} />,
+    <AiOutlineHtml5 key='2'{...props} />,
     <DiJavascript1 key='3'{...props} />,
     <SiMongodb key='4'{...props} />,
     <SiExpress key='5'{...props} />,
@@ -17,11 +17,13 @@ let logos = [
     <FaNodeJs key='7'{...props} />,
     <SiVisualstudiocode key='8'{...props} />,
     <SiNextdotjs key='9'{...props} />,
-
     <SiRedux key='10'{...props} />,
     <FaSass key='11'{...props} />,
     <SiMaterialui key='12'{...props} />,
-    
+    <DiGit key='13'{...props} />,
+    <DiHeroku key='141'{...props} />,
+    <SiVercel key='15'{...props} />,
+
 
 ]
 
@@ -57,16 +59,63 @@ const HeroAnimation = () => {
     //     // });
     // }, [])
     let container = useRef()
+    let container2 = useRef()
+    let timer = 12000
     const [num, setNum] = useState([1])
-    useEffect(() => {
+
+    const start = () => {
+        container.current.classList.remove('start')
+        container.current.classList.remove('init')
         setTimeout(() => {
-            console.log(num)
-            setNum(state => [...state, (state[state.length - 1] + 1)])
-            let newPos = (num.length - 1) * 400
-            console.log(newPos)
-            container.current.style.transform = `translateY(-${String(newPos)}px)`
-        }, 5000)
-    },[num])
+            container.current.classList.add('start-animate')
+        }, 5)
+        // container.current.classList.add('start-animate')
+        setInterval(() => {
+            container.current.classList.toggle('start-animate')
+        }, (timer))
+        setInterval(() => {
+            container.current.classList.toggle('start-animate')
+        }, (timer + 5))
+    }
+    const reset = () =>{
+        setTimeout(()=>{
+
+        container2.current.classList.remove('start-animate')
+    },1)
+
+        setTimeout(()=>{
+            container2.current.classList.add('start-animate')
+
+        },10)
+
+    }
+    useEffect(() => {
+        // container.current.classList.add('start-animate2-init')
+
+        container.current.classList.add('start')
+        // container2.current.classList.remove('init')
+        container2.current.classList.add('start-animate')
+
+        setTimeout(() => {
+            start()
+        }, timer / 2)
+        setInterval(() => {
+            reset()
+            // container2.current.classList.toggle('start-animate')
+        }, (timer))
+        // setInterval(() => {
+        //     container2.current.classList.toggle('start-animate')
+        // }, (timer + 10))
+    })
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log(num)
+    //         setNum(state => [...state, (state[state.length - 1] + 1)])
+    //         let newPos = (num.length - 1) * 300
+    //         console.log(newPos)
+    //         container.current.style.transform = `translateY(-${String(newPos)}px)`
+    //     }, timer)
+    // },[num])
     return (
         <>
             {/* <Button onClick={() => {
@@ -76,16 +125,30 @@ const HeroAnimation = () => {
                 console.log(newPos)
                 container.current.style.transform = `translateY(-${String(newPos)}px)`
             }}>ADD</Button> */}
-            <Box className='hero-animation' sx={{ position: 'relative', flex: '25% 1 1',maxWidth:'300px', height: {xs:'150px',md:'500px'}, overflowY: 'hidden' }}>
-                <Box className='tiles-container' ref={container} sx={{ transition: 'all 5000ms linear', position: 'relative', transform: 'translateY(00px)' }}>
-                    {num.map((anum,index) => (
-                    <Box key={index} className='tile' sx={{ display: 'flex', flexWrap: 'wrap' }}>
-                        {logos.map((component, index) => (
-                            <Box key={index} sx={{ p: 2, display: 'flex', flex: '80px 1 1', m: 1 }}>
-                                {component}
-                            </Box>
-                        ))}
-                    </Box>))}
+            <Box className='hero-animation' sx={{
+                position: 'relative', flex: '25% 1 1', maxWidth: '300px', height: { xs: '150px', md: '300px' },
+                 overflowY: 'hidden'
+                // border: '1px solid blue'
+            }}>
+                <Box className='tiles-container init ' ref={container} sx={{ transition: 'all 0ms linear', position: 'absolute', transform: 'translateY(100%)' }}>
+                    {num.map((anum, index) => (
+                        <Box key={index} className='tile' sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                            {logos.map((component, index) => (
+                                <Box key={index} sx={{ p: 2, display: 'flex', flex: '80px 1 1', m: 1 }}>
+                                    {component}
+                                </Box>
+                            ))}
+                        </Box>))}
+                </Box>
+                <Box className='tiles-container ' ref={container2} sx={{ transition: 'all 0ms linear', position: 'absolute', transform: 'translateY(100%)' }}>
+                    {num.map((anum, index) => (
+                        <Box key={index} className='tile' sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                            {logos.map((component, index) => (
+                                <Box key={index} sx={{ p: 2, display: 'flex', flex: '80px 1 1', m: 1 }}>
+                                    {component}
+                                </Box>
+                            ))}
+                        </Box>))}
                 </Box>
             </Box>
         </>
